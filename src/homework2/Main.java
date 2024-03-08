7441ebc6c72ffa8bca13bf0acf7475659f815685
gabri <gabriellondero40@gmail.com>
2024-03-07 22:27:37 -0300

    agregando la interface Map

--- src/homework2/Main.java
+++ src/homework2/Main.java
@@ -1,25 +1,12 @@
 package homework2;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
 
-        PartyGuestList listaInvitados = new PartyGuestList();
 
-        listaInvitados.addGuest("Florencia");
-        listaInvitados.addGuest("Gabriel");
-        listaInvitados.addGuest("Gianfranco");
-        listaInvitados.addGuest("fiorella");
-        listaInvitados.printGuests();
-        System.out.println(" ");
-        System.out.println("Invitados despues de borrar");
-        listaInvitados.removeGuest("Gianfranco");
-        System.out.println(" ");
-        listaInvitados.printGuests();
-        System.out.println(" ");
-        System.out.println("imprimos la total de invitados");
-        listaInvitados.getTotalGuests();
-        System.out.println("imprimos si esta en la lista");
-        listaInvitados.isGuestInList("Gabriel");
-        listaInvitados.isGuestInList("Lucas");
+
     }
 }
---
+++ src/homework2/StudentRecord.java
@@ -0,0 +1,24 @@
+package homework2;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class StudentRecord {
+    Map<String, Integer> listaAlumnos = new HashMap<>();
+
+    public void addStudent(String alumno, int calificacion){
+        listaAlumnos.put(alumno, calificacion);
+    }
+    public void removeStudent(String alumno){
+        System.out.println("Alumno: " + alumno + "eliminado con exito");
+        listaAlumnos.remove(alumno);
+    }
+    public void getGrade(String alumno) {
+        System.out.println(listaAlumnos.get(alumno));
+    }
+    public void isStudentInRecord(String alumno){
+        System.out.println(listaAlumnos.containsKey(alumno));
+    }
+}
