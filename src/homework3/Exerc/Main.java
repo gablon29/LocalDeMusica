88d1b5235e9a6135ec3ee7ba39bcba11f9589a17
gabri <gabriellondero40@gmail.com>
2024-03-09 18:56:04 -0300

    terminando la homework

--- src/homework3/Exerc/Main.java
+++ src/homework3/Exerc/Main.java
@@ -1,5 +1,6 @@
 package homework3.Exerc;
 
+import java.security.spec.RSAOtherPrimeInfo;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,5 +11,15 @@ public class Main {
         listaPersonas.agregarPersonas(new Persona("gabriel", "29", "programar"));
         listaPersonas.agregarPersonas(new Persona("Ivan", "28", "entrenar"));
         listaPersonas.agregarPersonas(new Persona("Florencia", "27", "programar"));
+        listaPersonas.agregarPersonas(new Persona("lucas", "23", "trabajar"));
+
+        List<Persona> personas = listaPersonas.filtroPorCantidad(2);
+        personas.forEach(persona -> System.out.println(persona.nombre));
+        List<String> nombres = listaPersonas.listarNombres();
+        nombres.forEach(System.out::println);
+        System.out.println(" ");
+        listaPersonas.printPersonas();
+        }
+
     }
-}
+
