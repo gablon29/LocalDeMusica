60cfdfa79456a8c6e585a527ae99f5dd30b1f678
gablon29 <gabriellondero40@gmail.com>
2024-05-21 11:56:07 -0300

    incorporando test unitarios para el funcionamiento

---
+++ src/exerciseTest/Operaciones.java
@@ -0,0 +1,12 @@
+package exerciseTest;
+
+public class Operaciones {
+
+    public static int areaRectangulo(int a, int b){
+        return a * b;
+    }
+    public static boolean esPar(int a){
+       if (a % 2 == 0) return true;
+       return false;
+    }
+}
